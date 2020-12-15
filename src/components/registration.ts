c933fc6722a31d3f47802f39f882784859006350
Anatoly <googlsocial97@gmail.com>
2020-12-15 23:00:58 +0700

    Update registration.ts

--- src/components/registration.ts
+++ src/components/registration.ts
@@ -1,9 +1,9 @@
 import Vue from "vue";
 
-import VButton from "@/components/v-button.vue"; // v-button.vue
+import VButton from "./v-button.vue"; // v-button.vue
 Vue.component("v-button", VButton);
 
-import VInputText from "@/components/v-input-text.vue"; // v-input-text.vue
+import VInputText from "./v-input-text.vue"; // v-input-text.vue
 Vue.component("v-input-text", VInputText);
 
 export default {
