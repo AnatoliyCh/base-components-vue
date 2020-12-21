3fce6320d9a491f03954c5b610ec1c7981226d09
Anatoly <googlsocial97@gmail.com>
2020-12-22 01:59:29 +0700

    Update registration.ts

--- src/base/registration.ts
+++ src/base/registration.ts
@@ -10,13 +10,13 @@ import VBlock from "./components/v-block.vue"; // v-input-text.vue
 Vue.component("v-block", VBlock);
 
 import VSpiner from "./components/v-spiner.vue"; // v-spiner.vue
-Vue.component("v-block", VBlock);
+Vue.component("v-spiner", VSpiner);
 
 export default {
   components: {
     VButton,
     VInputText,
     VBlock,
-    VSpiner
-  }
+    VSpiner,
+  },
 };
