9aa985b50f24c0310629f4bad1c5d583a2f69fbd
Anatoly <googlsocial97@gmail.com>
2020-12-23 04:01:16 +0700

    lint

--- src/base/components/v-input-password.vue
+++ src/base/components/v-input-password.vue
@@ -1,8 +1,7 @@
-<template src="./v-input-text.vue" ></template>
+<template src="./v-input-text.vue"></template>
 
 <script lang="ts">
-import { Component, Model } from "vue-property-decorator";
-import { mixins } from "vue-class-component";
+import { Component } from "vue-property-decorator";
 import VInputText from "./v-input-text.vue";
 
 @Component({ inheritAttrs: true })
